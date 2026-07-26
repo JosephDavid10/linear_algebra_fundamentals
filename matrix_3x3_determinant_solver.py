d4632a81e2c7f0f1139a1819a4baff44590bd4df
JosephDavid10 <jose.david.ssma@gmail.com>
2026-07-26 11:39:09 -0300

    Create matrix_3x3_determinant_solver.py
    function to solve 2x2 and 3x3 matrices determinants.

---
+++ matrix_3x3_determinant_solver.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def matrix_det(matrix):
+    plus = 0.0
+    minus = 0.0
+
+    if len(matrix) != len(matrix.T):
+        print('Error. Non-squared matrix has no determinant.')
+        return np.nan
+        
+    else:
+        if len(matrix) == 2:    #2x2 matrix
+            plus = matrix[0,0]*matrix[1,1]
+            minus = matrix[1,0]*matrix[0,1]
+            
+        else: 
+            for i in range(len(matrix)):  #3x3 matrix
+                positive_term = 1.0
+                negative_term = 1.0
+                
+                for j in range(len(matrix)):
+                    d = (i + j) % len(matrix) 
+                    positive_term *= matrix[j, d]
+                    negative_term *= matrix[j, -d]
+                    
+                plus += positive_term
+                minus += negative_term
+
+        return plus - minus
